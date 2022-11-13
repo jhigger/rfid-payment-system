8556f6728b07a6af3be4c1d665397fb61477ed2a
jhigger <njohnjhigger@gmail.com>
2022-11-14 06:39:44 +0800

    add auth context

---
+++ src/context/AuthContext.tsx
@@ -0,0 +1,63 @@
+import {
+	createUserWithEmailAndPassword,
+	onAuthStateChanged,
+	signInWithEmailAndPassword,
+	signOut,
+	type User,
+	type UserCredential
+} from "firebase/auth";
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth } from "../lib/firebase";
+
+interface AuthValues {
+	currentUser: User | null;
+	register: (email: string, password: string) => Promise<UserCredential>;
+	login: (email: string, password: string) => Promise<UserCredential>;
+	logout: () => Promise<void>;
+}
+
+const AuthContext = createContext<AuthValues>({} as AuthValues);
+
+const AuthProvider = ({ children }: { children: JSX.Element | null }) => {
+	const [currentUser, setCurrentUser] = useState<User | null>(null);
+	const [loading, setLoading] = useState(true);
+
+	const register = (email: string, password: string) => {
+		return createUserWithEmailAndPassword(auth, email, password);
+	};
+
+	const login = (email: string, password: string) => {
+		return signInWithEmailAndPassword(auth, email, password);
+	};
+
+	const logout = () => {
+		return signOut(auth);
+	};
+
+	useEffect(() => {
+		const unsubscribe = onAuthStateChanged(auth, async (user) => {
+			setCurrentUser(user);
+			setLoading(false);
+		});
+		return unsubscribe;
+	}, []);
+
+	const value = {
+		currentUser,
+		register,
+		login,
+		logout,
+	};
+
+	return (
+		<AuthContext.Provider value={value}>
+			{!loading && children}
+		</AuthContext.Provider>
+	);
+};
+
+const useAuth = () => {
+	return useContext(AuthContext);
+};
+
+export { AuthContext, AuthProvider, useAuth };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,16 @@
 import { type AppType } from "next/dist/shared/lib/utils";
+import { AuthProvider } from "../context/AuthContext";
 import { FirestoreProvider } from "../context/FirestoreContext";
 
 import "../styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
 	return (
-		<FirestoreProvider>
-			<Component {...pageProps} />
-		</FirestoreProvider>
+		<AuthProvider>
+			<FirestoreProvider>
+				<Component {...pageProps} />
+			</FirestoreProvider>
+		</AuthProvider>
 	);
 };
 
