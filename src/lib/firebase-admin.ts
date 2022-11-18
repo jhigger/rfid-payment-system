6178d8b91729c2a23093bbde559992db902a23f8
jhigger <njohnjhigger@gmail.com>
2022-11-18 09:30:47 +0800

    edit

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -3,6 +3,20 @@ import admin from "firebase-admin";
 
 const serviceAccount = process.env.NEXT_PUBLIC_FIREBASE_ADMIN as ServiceAccount;
 
-export default admin.initializeApp({
-	credential: admin.credential.cert(serviceAccount),
-});
+try {
+	admin.initializeApp({
+		credential: admin.credential.cert(serviceAccount),
+	});
+	console.log("Admin Initialized.");
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+} catch (error: any) {
+	/*
+	 * We skip the "already exists" message which is
+	 * not an actual error when we're hot-reloading.
+	 */
+	if (!/already exists/u.test(error.message)) {
+		console.error("Firebase admin initialization error", error.stack);
+	}
+}
+
+export default admin;
