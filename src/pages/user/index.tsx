d18be45bb96f18f7ded81261f90f1860a18a9d47
jhigger <njohnjhigger@gmail.com>
2022-12-10 06:44:47 +0800

    change button hover bg

--- src/pages/user/index.tsx
+++ src/pages/user/index.tsx
@@ -100,7 +100,7 @@ const SearchUserPage = () => {
 							<div className="my-4 flex w-full">
 								<button
 									type="submit"
-									className="w-full rounded-lg  bg-[#02A66D] py-2 px-4 text-center text-base font-semibold text-white shadow-md transition duration-200 ease-in hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2  focus:ring-offset-purple-200 "
+									className="w-full rounded-lg  bg-[#02A66D] py-2 px-4 text-center text-base font-semibold text-white shadow-md transition duration-200 ease-in hover:bg-[#006400] focus:outline-none focus:ring-2 focus:ring-purple-500 focus:ring-offset-2  focus:ring-offset-purple-200 "
 									disabled={isLoading}
 								>
 									{isLoading ? (
